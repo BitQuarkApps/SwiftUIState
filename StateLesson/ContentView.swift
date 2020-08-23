dbc2b25eee12eb1fe4262b8b76398451cd1b957a
Ingeniero Fernando <fernando@bitquark.com.mx>
2020-08-23 00:22:15 -0500

    Initial Commit

---
+++ StateLesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateLesson
+//
+//  Created by Ingeniero Fernando on 23/08/20.
+//  Copyright © 2020 Ingeniero Fernando. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
